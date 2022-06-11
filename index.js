1289ac4b7b53ebbee8319e64680d0df4c5ab30b4
Mwanasiti <ali.mariam.mwanasiti@gmail.com>
2022-06-11 22:13:15 +0300

    commit tree 70e316e6917535e5e80ecebe68e21e09442c5ace

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your code here!
+//const element = document.createElement("div");
+
+//document.body.append(element);
+
+//const ul = document.createElement("ul");
+
+//for (let i = 0; i < 3; i++) {
+  //const li = document.createElement("li");
+  //li.textContent = (i + 1).toString();
+  //ul.append(li);
+//}
+
+//element.append(ul);//
+
+//const element = document.getElementById("main");
+//element.style.height = "300px";
+//element.style.backgroundColor = "#27647B";
+
+const getElementById = "main"
+
+document.querySelector("main#main").remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Mariam is the champion!";
+newHeader.className = "victory";
